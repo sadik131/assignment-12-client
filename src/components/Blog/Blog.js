247f172b8f7b8734f0faa136c9ff31a2087d2f6e
supto <salatussadik131@gmail.com>
2022-07-31 16:46:02 +0600

    added login inpliment

--- src/components/Blog/Blog.js
+++ src/components/Blog/Blog.js
@@ -5,7 +5,7 @@ const Blog = () => {
         <div className='grid grid-cols-1 md:grid-cols-3 lg:grid-cols-3 gap-4 w-11/12 mx-auto my-7'>
             <div className='rounded-lg shadow-lg bg-white p-3 text-justify'>
                 <h1 className='text-xl font-bold'>Improve the performance of a React Application</h1>
-                <p>Keeping component state local where necessary. Memoizing React components to prevent unnecessary re-renders. Code-splitting in React using dynamic import Windowing or list virtualization in React. Lazy loading images in React.</p>
+                <p>Keeping component state local where necessary. Memoizing React components to prevent unnecessary re-renders. Code-splitting in React using dynamic import Windowing or list virtualization in React. Lazy loading images in React</p>
             </div>
             <div className='rounded-lg shadow-lg bg-white p-3 text-justify'>
                 <h1 className='text-xl font-bold'>Different ways to manage a state in a React application</h1>
